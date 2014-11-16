7d6d6444997e0cd56416b74ad6be544b08c85ca5
Aaron Williamson <wikea1@gmail.com>
2014-11-15 23:03:08 -0800

    create app

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var cityText: UITextField!
+    
+    @IBOutlet weak var weatherDescription: UILabel!
+    
+    @IBAction func showButton(sender: AnyObject) {
+        
+        self.view.endEditing(true)
+        
+        var noSpacesCityText = cityText.text.stringByReplacingOccurrencesOfString(" ", withString: "-")
+        var safeCityText = noSpacesCityText.stringByReplacingOccurrencesOfString(".", withString: "")
+        
+        var UrlString = "http://www.weather-forecast.com/locations/" + safeCityText + "/forecasts/latest"
+        let url = NSURL(string: UrlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+                
+
+            var resultData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            if (resultData!.containsString("<span class=\"phrase\">")) {
+                
+                var resultArray = resultData?.componentsSeparatedByString("<span class=\"phrase\">")
+                
+                var firstResultArray = resultArray![1].componentsSeparatedByString("</span>")
+                
+                var finalOutput = firstResultArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.weatherDescription.text = finalOutput
+                }
+                
+            } else {
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.weatherDescription.text = "No weather data for this city."
+                }
+                
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        cityText.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
